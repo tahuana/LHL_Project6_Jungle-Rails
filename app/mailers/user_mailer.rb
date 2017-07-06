cb3a4c0d4bd84e34aa9a300f4eb2c4c4a1c683d2
tahuana <tahuana@gmail.com>
2017-07-06 22:04:19 +0000

    Including details of the order in email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,12 @@
 class UserMailer < ApplicationMailer
 
-  def order_email(user, order)
+  def order_email(user_name, user_email, order, total_amount)
 
-    @user = user
-    @order  = order
-    mail(to: @user.email, subject: "Thank you for your order ##{@order.id}")
+      @user_name = user_name
+      @order = order
+      @total_amount = total_amount
+      mail(to: user_email, subject: "Thank you for your order ##{@order.id}")
+
   end
 
 end
