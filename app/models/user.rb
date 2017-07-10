7bf9b7e763823c0894fb911b430f679e6b62d5a9
tahuana <tahuana@gmail.com>
2017-07-10 19:50:23 +0000

    Implementing a new authentication method (class method authenticate_with_credentials)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ActiveRecord::Base
   validates :email, :uniqueness => {:case_sensitive => false}
   validates :password, :password_confirmation, length: { minimum: 4 }
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
 end
+
